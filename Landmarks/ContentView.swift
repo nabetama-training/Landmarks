e166f4e0cec6cce9daf733a565c3c91caaa65712
nabetama <mao.nabeta@gmail.com>
2023-10-07 22:58:34 +0900

    section 3

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rodk")
                 .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
+        .padding()
     }
 }
 
