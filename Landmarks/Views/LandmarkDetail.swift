c35a4329bdb03a65d676d4f8ce9bb0b4b37f3bca
nabetama <mao.nabeta@gmail.com>
2023-10-15 12:57:51 +0900

    section 6

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by nabetama on 2023/10/15.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack{
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom,  -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptice text goes here")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
